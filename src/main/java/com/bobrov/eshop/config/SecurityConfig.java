281062541e1c10aeb7a1f415881b6c20368a9478
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-10-12 19:20:35 +0300

    added rememberMe

--- src/main/java/com/bobrov/eshop/config/SecurityConfig.java
+++ src/main/java/com/bobrov/eshop/config/SecurityConfig.java
@@ -1,27 +1,57 @@
 package com.bobrov.eshop.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final DataSource dataSource;
+    @Value("${rememberMeKey}")
+    private String rememberMeKey;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setCreateTableOnStartup(false);
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeRequests()
-                .mvcMatchers("/", "").hasRole("ADMIN")
+                .mvcMatchers("/", "/login", "/registrate").permitAll()
 
                 .and().formLogin()
                 .loginPage("/login")
+
+                .and().rememberMe()
+                .key(rememberMeKey)
+                .tokenRepository(tokenRepository())
+
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+//                .deleteCookies("JSESSIONID")
+//                .invalidateHttpSession(true)
 
                 .and().build();
     }
