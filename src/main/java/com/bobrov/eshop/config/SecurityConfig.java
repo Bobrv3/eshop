4590de2882dc533cc612d2fc8d4b0970afa71e43
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-10-13 18:06:01 +0300

    config dev and prod profiles

--- src/main/java/com/bobrov/eshop/config/SecurityConfig.java
+++ src/main/java/com/bobrov/eshop/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bobrov.eshop.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,8 +37,38 @@ public class SecurityConfig {
         return jdbcTokenRepository;
     }
 
+    @Profile("dev")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeRequests()
+                .mvcMatchers("/", "/login", "/registrate").permitAll()
+
+                .and().formLogin()
+                .loginPage("/login")
+
+                .and().rememberMe()
+                .key(rememberMeKey)
+                .tokenRepository(tokenRepository())
+
+                .and().headers()
+                .frameOptions().sameOrigin()
+
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.ALL)))
+                .permitAll()
+
+                .and().csrf().disable()
+                .build();
+    }
+
+    @Profile("prod")
+    @Bean
+    public SecurityFilterChain filterChainP(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeRequests()
                 .mvcMatchers("/", "/login", "/registrate").permitAll()
